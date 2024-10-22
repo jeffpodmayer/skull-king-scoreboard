386b5546f689b1438c946e4a13cf07effc0c2560
jeffpodmayer <jeff.podmayer@gmail.com>
2024-10-22 11:44:01 -0700

    working through the struggle

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,17 @@ import { Hero } from "./components/Hero/Hero";
 import "./App.css";
 
 function App() {
-  const [players, setPlayers] = useState<{ name: string; score: number }[]>([]);
+  const [players, setPlayers] = useState<{ name: string; score: number }[]>([]); // change into an array of strings to jsut be an array of playernames
   const [rounds, setRounds] = useState<
     { roundNumber: number; scores: number[] }[]
   >([]);
   const [roundNumber, setRoundNumber] = useState(1);
-  // const [roundScores, setRoundScores] = useState<
-  //   { roundNumber: number; roundScore: number }[]
-  // >([]);
 
-  // Updates Players across all components when a new player is added
   const handleAddPlayer = (newPlayerName: string) => {
     setPlayers((prevPlayers) => [
       ...prevPlayers,
       { name: newPlayerName, score: 0 },
     ]);
-  };
-
-  //Ensure that bids are updated
-  const handleUpdateBid = (playerIndex: number, newBid: number) => {
-    setPlayers((prevPlayers) =>
-      prevPlayers.map((player, index) =>
-        index === playerIndex ? { ...player, bid: newBid } : player
-      )
-    );
   };
 
   // Updates the Round when a score is submitted
@@ -39,16 +26,18 @@ function App() {
       ...prevRounds,
       { roundNumber, scores: newScores },
     ]);
-
-    setPlayers((prevPlayers) =>
-      prevPlayers.map((player, index) => ({
-        ...player,
-        score: player.score + newScores[index],
-      }))
-    );
-
     setRoundNumber((prev) => prev + 1);
   };
+
+  // // could get rid of this and move it into the scoreboard component
+  // // conside only storing the round scores - compute player score using the rounnd scores on the fly
+  // // extract the compting fro overall player scores into it's own file
+  // setPlayers((prevPlayers) =>
+  //   prevPlayers.map((player, index) => ({
+  //     ...player,
+  //     score: player.score + newScores[index],
+  //   }))
+  // );
 
   return (
     <div>
