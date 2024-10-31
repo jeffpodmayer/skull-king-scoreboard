9bcb033bcf72b5df35ee540a81d7852c619802eb
jeffpodmayer <jeff.podmayer@gmail.com>
2024-10-31 08:38:51 -0700

    added capacitor for ios seeing error due to Mac version

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.skullkingscoreboard.app",
+  appName: "Skull King Scorekeeper",
+  webDir: "dist",
+};
+
+export default config;
